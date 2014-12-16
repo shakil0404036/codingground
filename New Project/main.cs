7280d42b20af5ec324044b2480d1069843d55cae
Md Shakil Ahamed <md.shakil.ahamed.cse@gmail.com>
2014-12-16 01:02:11 -0500

    Created by Coding Ground Client

---
+++ New Project/main.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        string man = "do something";
+        for(int i=0; i<man.Length; i++)
+           Console.Write(man[i]+" ");
+           Console.WriteLine("Change!! huhaha..");
+           
+    }
+}
